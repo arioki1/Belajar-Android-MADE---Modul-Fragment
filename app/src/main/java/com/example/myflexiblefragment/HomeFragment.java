c6d7c1680caa5e23e27fe5d719fa4e7af8233e33
Yoga Setiawan <mryoga1995@gmail.com>
2019-05-09 10:13:51 +0700

    Fragment - Latihan : Fragment Sederhana

---
+++ app/src/main/java/com/example/myflexiblefragment/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.myflexiblefragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment implements View.OnClickListener{
+
+    View v;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        Button btnCategory = v.findViewById(R.id.btn_category);
+        btnCategory.setOnClickListener(this);
+
+        return v;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
